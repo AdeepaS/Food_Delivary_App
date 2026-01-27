cfad51417d09f8c11ada5bef646cd04e2555812f
AdeepaS <adeepashashiprabhath@gmail.com>
2026-01-27 23:19:24 +0530

    add styles

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,6 +4,10 @@ import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+
+  
+
+
   return (
     <div>
       <Header/>
